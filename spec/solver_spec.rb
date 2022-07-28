49ce5ba683d91c1de3b47c59880488cd75004634
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-07-28 12:56:15 -0600

    Added fizzbuzz test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -18,7 +18,27 @@ describe Solver do
   
       it 'Reverses a string with the reverse method' do
         expect(solver.reverse('hello')).to eq('olleh')
-        expect(solver.reverse('testing')).to eq('gnitset')
+        expect(solver.reverse('microverse')).to eq('esrevorcim')
+      end
+
+      it 'Fizzbuzz method returns "fizz" when a multiple of 3 but not 5 is passed ' do
+        expect(solver.fizzbuzz(3)).to eq('fizz')
+        expect(solver.fizzbuzz(81)).to eq('fizz')
+      end
+  
+      it 'Fizzbuzz method returns "buzz" when a multiple of 5 but not 3 is passed' do
+        expect(solver.fizzbuzz(125)).to eq('buzz')
+        expect(solver.fizzbuzz(25)).to eq('buzz')
+      end
+  
+      it 'Fizzbuzz method returns "fizzbuzz" when a multiple of 3 and 5 is passed' do
+        expect(solver.fizzbuzz(30)).to eq('fizzbuzz')
+        expect(solver.fizzbuzz(45)).to eq('fizzbuzz')
+      end
+  
+      it 'Fizzbuzz method returns a number that is not a multiple of 3 and 5 as a string' do
+        expect(solver.fizzbuzz(1)).to eq('1')
+        expect(solver.fizzbuzz(7)).to eq('7')
       end
     end
   end
