9faf6a65b9c36cea64d0ef92fb7be1e22e32e481
Adaobi <97225349+Adanzeakonobi@users.noreply.github.com>
2022-07-28 13:03:22 -0600

    Created factorial method

--- solver.rb
+++ solver.rb
@@ -1,3 +1,9 @@
 class Solver
-
+def factorial(number)
+    return 'No negative number allowed' if number.negative?
+    if number.zero?
+        1
+    else
+        number * factorial(number - 1)
+    end
 end
